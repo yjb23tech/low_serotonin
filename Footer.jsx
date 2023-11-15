54c7bdcb7ac127679d59492776391477b04a6893
yjb23tech <yjb23tech@gmail.com>
2023-11-15 19:41:52 +0000

    Prometheus Rising

---
+++ Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer>
+            <small>2023 NoirLabs development. All rights reserved.</small>
+        </footer>
+    );
+};
+
+export default Footer; 
---
+++ Header.jsx
@@ -0,0 +1,16 @@
+const Header = () => {
+    return (
+        <header>
+            <nav className="nav">
+                <img src="./react-log.png" className="nav-log" />
+                <ul className="nav-items">
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header; 
